eadedbc961ee597e950ab71b404ef491fbb4737c
Mike Kornegay <mjkorneg@gmail.com>
2024-12-01 09:16:00 -0500

    Add files via upload

---
+++ Sf.Budget.Core/Interfaces/IAggregateRoot.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sf.Budget.Core.Interfaces
+{
+    /// <summary>
+    /// Marker interface for aggregate root
+    /// </summary>
+    public interface IAggregateRoot
+    { }
+}
---
+++ Sf.Budget.Core/Interfaces/IEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sf.Budget.Core.Interfaces
+{
+    /// <summary>
+    /// Base interface for all entities
+    /// </summary>
+    public interface IEntity
+    {
+    }
+
+    public interface IEntity<out TKey> : IEntity where TKey : IEquatable<TKey>
+    {
+        TKey Id { get; }
+    }
+}
